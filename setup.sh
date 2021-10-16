f48b8ee38318f840975b7b99a73f6f0f2efa238f
Thuan Le <thuanle.ute@gmail.com>
2021-10-16 14:31:19 -0700

    update: Build

--- setup.sh
+++ setup.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
 
-# CURR_DIR=${PWD}
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CURR_SHELL="$(basename -- $SHELL)"
 SHELL_CFG="$HOME/.${CURR_SHELL:-bash}rc"
 XTENSA_PATH="sdk/xtensa-esp32-elf/bin"
+DEFAULT_IDF_PATH="$CURR_DIR/sdk/esp-idf"
 
+# Install required packages
+sudo apt-get install python3-pip
 sudo apt-get install -y gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future python-pyparsing libffi-dev libssl-dev
 
+# Make sure $USER have access to USB port
+sudo usermod -a -G dialout $USER
+
+# Update repo submodules
 git submodule update --init --recursive
 
 echo "INFO: CURR_SHELL = $CURR_SHELL"
 echo "INFO: SHELL_CFG = $SHELL_CFG"
 
-# Check if IDF_PATH existed or not
-if [[ -z "${IDF_PATH}" ]];
-then
-    echo "INFO: IDF_PATH NOT FOUND. Exporting..."
-    echo -e "export IDF_PATH=$CURR_DIR/sdk/esp-idf" >> "$SHELL_CFG"
-else
-    echo "INFO: IDF_PATH FOUND!"
-fi
-
 # Check if xtensa path is exported or not
 if [[ "$PATH" == *"$XTENSA_PATH"* ]]; then
-  echo "INFO: XTENSA FOUND"
+  echo "INFO: Xtensa Compiler FOUND"
 else
-  echo "INFO: XTENSA NOT FOUND. Exporting..."
+  echo "INFO: Xtensa Compiler NOT FOUND. Exporting..."
   echo -e "export PATH=\"\$PATH:$CURR_DIR/$XTENSA_PATH\"" >> "$SHELL_CFG"
 fi
 
-sudo usermod -a -G dialout $USER
-
-python -m pip install --user -r $CURR_DIR/sdk/esp-idf/requirements.txt
+# Check if IDF_PATH existed or not
+if [[ -z "${IDF_PATH}" ]];
+then
+  echo "INFO: IDF_PATH NOT FOUND. Exporting..."
+  echo -e "export IDF_PATH=$DEFAULT_IDF_PATH" >> "$SHELL_CFG"
+  
+  # Install Python requirements 
+  python2 -m pip install --user -r $DEFAULT_IDF_PATH/requirements.txt
+else
+  echo "INFO: IDF_PATH FOUND!"
+fi
 
 # Check if ADF_PATH existed or not
 if [[ -z "${ADF_PATH}" ]];
 then
-    echo "INFO: ADF_PATH NOT FOUND. Exporting..."
-    echo -e "export ADF_PATH=$CURR_DIR/sdk/esp-adf" >> "$SHELL_CFG"
-    bash $CURR_DIR/sdk/esp-idf/install.sh
-    echo -e "source $CURR_DIR/sdk/esp-idf/export.sh" >> "$SHELL_CFG"
+  echo "INFO: ADF_PATH NOT FOUND. Exporting..."
+  bash $DEFAULT_IDF_PATH/install.sh
+  echo -e "source $DEFAULT_IDF_PATH/export.sh" >> "$SHELL_CFG"
 else
-    echo "INFO: ADF_PATH FOUND!"
+  echo "INFO: ADF_PATH FOUND!"
 fi
-
-source $SHELL_CFG
 
 # ✔: \xe2\x9c\x93
 # ✗: \xe2\x9c\x97
-echo -e "\xe2\x9c\x93 Setup OK"
+echo -e "\xe2\x9c\x93 Setup OK. Please log out!"
